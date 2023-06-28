cdf3f9d94192ae1fa43576e094261ed70f6697bd
newtjss <sinn1901@Usherbrooke.ca>
2023-06-28 04:24:42 -0400

    test1
    page pr savoir pr quel event tu scannes

---
+++ usager/service/src/main/java/ca/usherbrooke/gegi/server/admin/Scanning.java
@@ -0,0 +1,11 @@
+public class Scanning {
+    public String evenementID;
+
+    public String getEvenementID() {
+        return evenementID;
+    }
+
+    public void setEvenementID(String evenementID) {
+        this.evenementID = evenementID;
+    }
+}
