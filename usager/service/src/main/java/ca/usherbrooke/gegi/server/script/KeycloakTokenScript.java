7e1258f63932f46b0183563d714cd459a1883d27
CAKE0801 <114595545+CAKE0801@users.noreply.github.com>
2023-07-18 20:12:06 -0400

    v2

--- usager/service/src/main/java/ca/usherbrooke/gegi/server/script/KeycloakTokenScript.java
+++ usager/service/src/main/java/ca/usherbrooke/gegi/server/script/KeycloakTokenScript.java
@@ -9,6 +9,8 @@ import java.io.FileOutputStream;
 import java.nio.charset.StandardCharsets;
 import java.sql.*;
 import java.util.Random;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,7 +34,7 @@ public class KeycloakTokenScript {
             Process process = Runtime.getRuntime().exec(command);
 
             // Read
-            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8));
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
             String line;
             StringBuilder response = new StringBuilder();
 
@@ -64,6 +66,8 @@ public class KeycloakTokenScript {
             conn.setRequestProperty("Authorization", "Bearer " + token);
             conn.setRequestProperty("Content-Type", "application/json");
             conn.setDoOutput(true);
+            String contentType = conn.getHeaderField("Content-Type");
+            System.out.println(contentType);
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             StringBuilder output = new StringBuilder();
@@ -95,6 +99,7 @@ public class KeycloakTokenScript {
                 userJson.put("groups", new JSONArray(groupsArray.toString()));
 
                 usersData.put(userJson);
+
             }
             System.out.println(usersData.toString(2));
         } catch (Exception e) {
@@ -111,8 +116,10 @@ public class KeycloakTokenScript {
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Authorization", "Bearer " + token);
             conn.setRequestProperty("Content-Type", "application/json");
+            conn.setRequestProperty("Accept-Charset", "UTF-8");
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             StringBuilder output = new StringBuilder();
             String line;
             while ((line = br.readLine()) != null) {
@@ -138,6 +145,7 @@ public class KeycloakTokenScript {
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Authorization", "Bearer " + token);
             conn.setRequestProperty("Content-Type", "application/json");
+
 
             BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8));
             StringBuilder output = new StringBuilder();
@@ -192,156 +200,6 @@ public class KeycloakTokenScript {
             e.printStackTrace();
         }
         return subGroupsArray;
-    }
-
-
-
-    public static void insertUniversite(String universiteNom) throws Exception {
-        try {
-            Connection con = getConnection();
-
-            String selectQuery = "SELECT * FROM base_de_donne.universite WHERE Universite_Nom = ?";
-            PreparedStatement selectPreparedStatement = con.prepareStatement(selectQuery);
-            selectPreparedStatement.setString(1, universiteNom);
-
-            ResultSet resultSet = selectPreparedStatement.executeQuery();
-
-            if(!resultSet.next()) {
-                String insertQuery = "INSERT INTO base_de_donne.universite (UniversiteID, Universite_Nom) VALUES (?, ?)";
-
-                PreparedStatement insertPreparedStatement = con.prepareStatement(insertQuery);
-
-                int universiteID = generateRandom4DigitKey();
-                insertPreparedStatement.setInt(1, universiteID);
-                insertPreparedStatement.setString(2, universiteNom);
-
-                insertPreparedStatement.executeUpdate();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void insertFaculty(String facultyName, String universityName) throws Exception {
-        try {
-            Connection con = getConnection();
-
-            String selectUniversityQuery = "SELECT UniversiteID FROM base_de_donne.universite WHERE Universite_Nom = ?";
-            PreparedStatement selectUniversityPreparedStatement = con.prepareStatement(selectUniversityQuery);
-            selectUniversityPreparedStatement.setString(1, universityName);
-
-            ResultSet universityResultSet = selectUniversityPreparedStatement.executeQuery();
-
-            if(universityResultSet.next()) {
-                int universityID = universityResultSet.getInt("UniversiteID");
-
-                String selectFacultyQuery = "SELECT 1 FROM base_de_donne.faculte WHERE Faculte_Nom = ? AND UniversiteID = ?";
-                PreparedStatement selectFacultyPreparedStatement = con.prepareStatement(selectFacultyQuery);
-                selectFacultyPreparedStatement.setString(1, facultyName);
-                selectFacultyPreparedStatement.setInt(2, universityID);
-
-                ResultSet facultyResultSet = selectFacultyPreparedStatement.executeQuery();
-
-                if(!facultyResultSet.next()) {
-                    String insertQuery = "INSERT INTO base_de_donne.faculte (FaculteID, Faculte_Nom, UniversiteID) VALUES (?, ?, ?)";
-
-                    PreparedStatement insertPreparedStatement = con.prepareStatement(insertQuery);
-
-                    int facultyID = generateRandom4DigitKey();
-                    insertPreparedStatement.setInt(1, facultyID);
-                    insertPreparedStatement.setString(2, facultyName);
-                    insertPreparedStatement.setInt(3, universityID);
-
-                    insertPreparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void insertUser(String userId, String lastName, String firstName, String role, String facultyName) {
-        try {
-            Connection con = getConnection();
-
-            String selectFacultyQuery = "SELECT FaculteID FROM base_de_donne.faculte WHERE Faculte_Nom = ?";
-            PreparedStatement selectFacultyPreparedStatement = con.prepareStatement(selectFacultyQuery);
-            selectFacultyPreparedStatement.setString(1, facultyName);
-
-            ResultSet facultyResultSet = selectFacultyPreparedStatement.executeQuery();
-
-            if(facultyResultSet.next()) {
-                int facultyID = facultyResultSet.getInt("FaculteID");
-
-                String selectUserQuery = "SELECT 1 FROM base_de_donne.usager WHERE UsagerID = ?";
-                PreparedStatement selectUserPreparedStatement = con.prepareStatement(selectUserQuery);
-                selectUserPreparedStatement.setString(1, userId);
-
-                ResultSet userResultSet = selectUserPreparedStatement.executeQuery();
-
-                if(!userResultSet.next()) {
-                    String insertQuery = "INSERT INTO base_de_donne.usager (UsagerID, Usager_Nom, Usager_Prenom, Usager_Role, FaculteID) VALUES (?, ?, ?, ?, ?)";
-
-                    PreparedStatement insertPreparedStatement = con.prepareStatement(insertQuery);
-
-                    insertPreparedStatement.setString(1, userId);
-                    insertPreparedStatement.setString(2, lastName);
-                    insertPreparedStatement.setString(3, firstName);
-                    insertPreparedStatement.setString(4, role);
-                    insertPreparedStatement.setInt(5, facultyID);
-
-                    insertPreparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    private static int generateRandom4DigitKey() {
-        Random random = new Random();
-        return random.nextInt(9000) + 1000;
-    }
-
-
-    public static boolean universiteExists(String universiteName) {
-        String query = "SELECT COUNT(*) FROM base_de_donne.universite WHERE Universite_Nom = ?";
-        try (Connection con = getConnection();
-             PreparedStatement preparedStatement = con.prepareStatement(query)) {
-
-            preparedStatement.setString(1, universiteName);
-            ResultSet rs = preparedStatement.executeQuery();
-
-            if (rs.next()) {
-                int count = rs.getInt(1);
-                return count > 0;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static boolean facultyExists(String facultyName, String universiteName) {
-        String query = "SELECT COUNT(*) FROM base_de_donne.faculte JOIN base_de_donne.universite ON faculte.UniversiteID = universite.UniversiteID WHERE faculte.Faculte_Nom = ? AND universite.Universite_Nom = ?";
-        try (Connection con = getConnection();
-             PreparedStatement preparedStatement = con.prepareStatement(query)) {
-
-            preparedStatement.setString(1, facultyName);
-            preparedStatement.setString(2, universiteName);
-            ResultSet rs = preparedStatement.executeQuery();
-
-            if (rs.next()) {
-                int count = rs.getInt(1);
-                return count > 0;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return false;
     }
 
 }
