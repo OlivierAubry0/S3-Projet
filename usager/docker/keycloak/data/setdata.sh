2bccfc7016f0ca6c47ba963309a85897876072ff
CAKE0801 <114595545+CAKE0801@users.noreply.github.com>
2023-07-19 17:41:46 -0400

    good

--- usager/docker/keycloak/data/setdata.sh
+++ usager/docker/keycloak/data/setdata.sh
@@ -5,8 +5,8 @@ if ! command -v jq &> /dev/null
 then
     echo "jq could not be found"
     echo "Installing jq..."
-    sudo apt-get update
-    sudo apt-get install -y jq
+    apt-get update
+    apt-get install -y jq
 fi
 
 timeout 300 /bin/bash -c 'until curl -sI -o /dev/null -w "%{http_code}\n" localhost:8180; do sleep 5; done;'
@@ -18,7 +18,6 @@ echo "beginning of finalisation ...."
 /opt/keycloak/bin/kcadm.sh create clients -r usager -f /var/tmp/backend.json
 /opt/keycloak/bin/kcadm.sh create partialImport -r usager -s ifResourceExists=OVERWRITE -f /var/tmp/users.json
 
-
 sherbrooke_group_id=$(/opt/keycloak/bin/kcadm.sh create groups -r usager -s name="Universite De Sherbrooke" -i)
 /opt/keycloak/bin/kcadm.sh create groups/$sherbrooke_group_id/children -r usager -s name="Faculte de Genie"
 /opt/keycloak/bin/kcadm.sh create groups/$sherbrooke_group_id/children -r usager -s name="Faculte de Gestion"
@@ -28,6 +27,15 @@ laval_group_id=$(/opt/keycloak/bin/kcadm.sh create groups -r usager -s name="Uni
 /opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Genie"
 /opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Medecine"
 
+echo "Retrieving user data..."
+user_data=$(/opt/keycloak/bin/kcadm.sh get users -r usager)
+echo "User data retrieval complete."
+
+bour0703_id=$(echo "$user_data" | grep -oP '(?<="id" : ")[^"]*')
+echo "The user ID for bour0703 is: $bour0703_id"
+
+
+/opt/keycloak/bin/kcadm.sh update users/$bour0703_id/groups/$sherbrooke_group_id -r usager -s realm=usager -s userId=$bour0703_id -s groupId=$sherbrooke_group_id -n
 
 echo -e -n "\r"
 echo "server running ...."
