85513cb8b0bfaabb40d5eb7c219365b425500751
CAKE0801 <114595545+CAKE0801@users.noreply.github.com>
2023-07-19 13:41:57 -0400

    test 1

--- usager/docker/keycloak/data/setdata.sh
+++ usager/docker/keycloak/data/setdata.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Install jq if not already installed
+if ! command -v jq &> /dev/null
+then
+    echo "jq could not be found"
+    echo "Installing jq..."
+    sudo apt-get update
+    sudo apt-get install -y jq
+fi
+
 timeout 300 /bin/bash -c 'until curl -sI -o /dev/null -w "%{http_code}\n" localhost:8180; do sleep 5; done;'
 
 echo "beginning of finalisation ...."
@@ -9,6 +18,16 @@ echo "beginning of finalisation ...."
 /opt/keycloak/bin/kcadm.sh create clients -r usager -f /var/tmp/backend.json
 /opt/keycloak/bin/kcadm.sh create partialImport -r usager -s ifResourceExists=OVERWRITE -f /var/tmp/users.json
 
+
+sherbrooke_group_id=$(/opt/keycloak/bin/kcadm.sh create groups -r usager -s name="Universite De Sherbrooke" -i)
+/opt/keycloak/bin/kcadm.sh create groups/$sherbrooke_group_id/children -r usager -s name="Faculte de Genie"
+/opt/keycloak/bin/kcadm.sh create groups/$sherbrooke_group_id/children -r usager -s name="Faculte de Gestion"
+/opt/keycloak/bin/kcadm.sh create groups/$sherbrooke_group_id/children -r usager -s name="Faculte de Droit"
+
+laval_group_id=$(/opt/keycloak/bin/kcadm.sh create groups -r usager -s name="Universite De Laval" -i)
+/opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Genie"
+/opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Medecine"
+
+
 echo -e -n "\r"
 echo "server running ...."
-
