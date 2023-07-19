0ceeda8a13e89293718866c10ad7e8ab813d151e
CAKE0801 <114595545+CAKE0801@users.noreply.github.com>
2023-07-19 19:36:31 -0400

    ee

--- usager/docker/keycloak/data/setdata.sh
+++ usager/docker/keycloak/data/setdata.sh
@@ -5,8 +5,9 @@ if ! command -v jq &> /dev/null
 then
     echo "jq could not be found"
     echo "Installing jq..."
-    apt-get update
-    apt-get install -y jq
+    sudo apt-get update
+    sudo apt-get install -y jq
+
 fi
 
 timeout 300 /bin/bash -c 'until curl -sI -o /dev/null -w "%{http_code}\n" localhost:8180; do sleep 5; done;'
@@ -27,15 +28,16 @@ laval_group_id=$(/opt/keycloak/bin/kcadm.sh create groups -r usager -s name="Uni
 /opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Genie"
 /opt/keycloak/bin/kcadm.sh create groups/$laval_group_id/children -r usager -s name="Faculte de Medecine"
 
-echo "Retrieving user data..."
-user_data=$(/opt/keycloak/bin/kcadm.sh get users -r usager)
-echo "User data retrieval complete."
+bour0703_info=$(/opt/keycloak/bin/kcadm.sh get users -r usager -q username=bour0703)
+bour0703_id=$(echo $bour0703_info | grep -Po '"id":.*?[^\\]",' | cut -d':' -f2 | tr -d ' ",')
 
-bour0703_id=$(echo "$user_data" | grep -oP '(?<="id" : ")[^"]*')
-echo "The user ID for bour0703 is: $bour0703_id"
+echo "bour0703's ID: $bour0703_id"
 
+master0102_info=$(/opt/keycloak/bin/kcadm.sh get users -r usager -q username=master0102)
+master0102_id=$(echo $master0102_info | grep -Po '"id":.*?[^\\]",' | cut -d':' -f2 | tr -d ' ",')
 
-/opt/keycloak/bin/kcadm.sh update users/$bour0703_id/groups/$sherbrooke_group_id -r usager -s realm=usager -s userId=$bour0703_id -s groupId=$sherbrooke_group_id -n
+echo "master0102's ID: $master0102_id"
+
 
 echo -e -n "\r"
 echo "server running ...."
