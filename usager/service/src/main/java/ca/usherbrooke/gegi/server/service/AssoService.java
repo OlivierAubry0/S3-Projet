28b9fdb1ad9a771848a06bce5add38178b8e604f
Pape Malick Thioye <thip0901@usherbrooke.ca>
2023-07-22 19:53:05 -0400

    nettoyage master

--- usager/service/src/main/java/ca/usherbrooke/gegi/server/service/AssoService.java
+++ usager/service/src/main/java/ca/usherbrooke/gegi/server/service/AssoService.java
@@ -23,8 +23,8 @@ public class AssoService {
     AssoMapper assomapper;
     @POST
     public Response createAsso(AssoEtudiante asso) {
-        // Generate a UUID and set it as the eventId
-        //asso.setAssoEtudianteID(UUID.randomUUID().toString());
+         //Generate a UUID and set it as the eventId
+        asso.setAssoEtudianteID(UUID.randomUUID().toString());
 
         assomapper.insertAsso(asso);
         return Response.status(Response.Status.CREATED).entity(asso).build();
@@ -43,25 +43,6 @@ public class AssoService {
         return assomapper.getAllAsso();
     }
 
-    /*
-        @POST
-        @Path("/Asso_EtudianteID")
-        @Produces(MediaType.APPLICATION_JSON)
-        public Response updateAsso(@QueryParam("Asso_EtudianteID") String Asso_EtudianteID, AssoEtudiante updatedAsso) {
-            AssoEtudiante asso = assomapper.getAssoById(Asso_EtudianteID);
-            if (asso == null) {
-                return Response.status(Response.Status.NOT_FOUND).build();
-            }
-
-            // Update the values of the existing association
-            asso.setAssoEtudianteNom(updatedAsso.getAssoEtudianteNom());
-            asso.setFaculteID(updatedAsso.getFaculteID());
-            asso.setUsagerID(updatedAsso.getUsagerID());
-
-            assomapper.update_the_Asso(asso);
-            return Response.status(Response.Status.OK).entity(asso).build();
-        }
-    */
     @POST
     @Path("/Asso_EtudianteID")
     @Produces(MediaType.APPLICATION_JSON)
